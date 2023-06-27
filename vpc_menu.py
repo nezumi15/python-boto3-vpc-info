254cfb9cfb6038266b5b4db35e72d73451aed7fd
nezumi15 <60234246+nezumi15@users.noreply.github.com>
2023-06-27 14:23:15 -0400

    Update and rename vpc_info.py to vpc_menu.py

--- vpc_menu.py
+++ vpc_menu.py
@@ -2,36 +2,40 @@ import sys
 
 def display_menu():
     print("Select an option:")
-    print("1. List NAT Gateways")
-    print("2. List S3 Endpoints")
-    print("3. Describe Main Route Table")
-    print("4. List Transit Gateways")
-    print("5. List VPN Gateways")
+    print("1. List VPC CIDRs")
+    print("2. List NAT Gateways")
+    print("3. List Main Routing Table")
+    print("4. List S3 Endpoints")
+    print("5. List Transit Gateways")
+    print("6. List VPN Gateways")
     print("0. Exit")
 
 def run_script(script_index):
     if script_index == 1:
-        import vpc_natgw
-        vpc_natgw.lambda_handler('event', 'context')
+        import vpc_vpc_cidrs
+        vpc_cidrs.lambda_handler('event', 'context')
     elif script_index == 2:
-        import vpc_s3_enpoints
-        vpc_s3_enpoints.lambda_handler('event', 'context')
+        import vpc_ngw
+        vpc_ngw.lambda_handler('event', 'context')
     elif script_index == 3:
         import vpc_rt
         vpc_rt.lambda_handler('event', 'context')
     elif script_index == 4:
+        import vpc_s3_endpoints
+        vpc_s3_endpoints.lambda_handler('event', 'context')
+    elif script_index == 5:
         import vpc_tgw
         vpc_tgw.lambda_handler('event', 'context')
-    elif script_index == 5:
-        import vpc_vgw
-        vpc_vgw.lambda_handler('event', 'context')
+    elif script_index == 6:
+        import vpc_vpg
+        vpc_vpg.lambda_handler('event', 'context')
     else:
         print("Invalid option. Please try again.")
 
 def main():
     while True:
         display_menu()
-        option = input("Enter your choice (0-5): ")
+        option = input("Enter your choice (0-6): ")
         try:
             option = int(option)
             if option == 0:
